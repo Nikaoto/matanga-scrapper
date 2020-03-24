aca19c8e5c94ba87d922d2b75479b8efeb0640a0
Nikaoto <nikaoto@gmail.com>
2020-03-25 00:24:34 +0400

    update data dir naming

--- run-scrapper.sh
+++ run-scrapper.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
+DATA_DIR="data"
 NOW_MONTH="$(date +%m)"
-mkdir -p "$NOW_MONTH"
-xvfb-run python3 scrape.py "$NOW_MONTH"
+SAVE_DIR="${DATA_DIR}/${NOW_MONTH}"
+mkdir -p "$SAVE_DIR"
+xvfb-run python3 scrape.py "$SAVE_DIR"
